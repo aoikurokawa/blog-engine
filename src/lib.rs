ee54ff1ef2658c9d84fdbdf7983118762bd0f647
Aoi <ht.so.ik0102@gmail.com>
2021-12-11 10:34:42 +0700

    post and clear

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,18 @@ struct IndexResponse {
     messages: Vec<String>,
 }
 
+#[derive(Deserialize)]
+struct PostInput {
+    message: String,
+}
+
+#[derive(Serialize)]
+struct PostResponse {
+    server_id: usize,
+    request_count: usize,
+    message: String,
+}
+
 #[get("/")]
 async fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
     let request_count = state.request_count.get() + 1;
@@ -32,6 +44,36 @@ async fn index(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
         server_id: state.server_id,
         request_count,
         messages: ms.clone(),
+    }))
+}
+
+async fn post(
+    msg: web::Json<PostInput>,
+    state: web::Data<AppState>,
+) -> Result<web::Json<PostResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messge.lock().unwrap();
+    ms.push(msg.message.clone());
+
+    Ok(web::Json(PostResponse {
+        server_id: state.server_id,
+        request_count,
+        message: msg.message.clone(),
+    }))
+}
+
+#[post("/clear")]
+fn clear(state: web::Data<AppState>) -> Result<web::Json<IndexResponse>> {
+    let request_count = state.request_count.get() + 1;
+    state.request_count.set(request_count);
+    let mut ms = state.messages.lock().unwrap();
+    ms.clear();
+
+    Ok(web::Json(IndexResponse {
+        server_id: state.server_id,
+        request_count,
+        messages: vec![],
     }))
 }
 
@@ -56,6 +98,12 @@ impl MessageApp {
                 })
                 .wrap(middleware::Logger::default())
                 .service(index)
+                .service(
+                    web::resource("/send")
+                        .data(web::JsonConfig::default().limit(4096))
+                        .route(web::post().to(post)),
+                )
+                .service(clear)
         })
         .bind(("127.0.0.1", self.port))?
         .workers(8)
