9073e2b586d15d15dd82632f823751f5efc08caf
Aoi <ht.so.ik0102@gmail.com>
2021-12-11 18:33:00 +0700

    update lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
-use messages_actix::MessageApp;
+use dotenv::dotenv;
+use std::env;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
     std::env::set_var("RUST_LOG", "actix-web=info");
     env_logger::init();
-    let app = MessageApp::new(8080);
-    app.run().await
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let app = messages-actix::Blog::new(8998);
+    // let app = MessageApp::new(8080);
+    app.run(database_url).await
 }
