7aaa7dc26e89d1496ab9070c08328d63b939a2bf
Aoi <ht.so.ik0102@gmail.com>
2021-12-12 10:11:12 +0700

    remove warning in errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -1,7 +1,7 @@
 use actix_web::error::BlockingError;
 use actix_web::web::HttpResponse;
 use diesel::result::DatabaseErrorKind::UniqueViolation;
-use diesel::result::Error::{DatabaseError, NotFound};
+use diesel::result::Error::{DatabaseError};
 use serde::Serialize;
 use std::fmt;
 
