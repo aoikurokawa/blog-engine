2ff869967f9fc1247454de44bc732026f99ea68a
Aoi <ht.so.ik0102@gmail.com>
2021-12-12 09:32:36 +0700

    declare user struct in User

---
+++ src/models.rs
@@ -0,0 +1,12 @@
+use crate::errors::AppError;
+use crate::schema::users;
+use diesel::prelude::*;
+use serde::Serialize;
+
+type Result<T> = std::result::Result<T, AppError>;
+
+#[derive(Queryable, Identifiable, Serialize, Debug, PartialEq)]
+pub struct User {
+    pub id: i32,
+    pub username: String,
+}
