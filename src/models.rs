1930f118c2f279de18a7406585bf1e7f88aa6589
Aoi <ht.so.ik0102@gmail.com>
2021-12-12 10:09:34 +0700

    find user function

--- src/models.rs
+++ src/models.rs
@@ -14,7 +14,7 @@ pub struct User {
 pub fn create_user(conn: &PgConnection, username: &str) -> Result<User> {
     conn.transaction(|| {
         diesel::insert_into(users::table)
-            .values((users::username.eq(username), ))
+            .values((users::username.eq(username),))
             .execute(conn)?;
 
         users::table
@@ -23,4 +23,25 @@ pub fn create_user(conn: &PgConnection, username: &str) -> Result<User> {
             .first(conn)
             .map_err(Into::into)
     })
+}
+
+pub enum UserKey<'a> {
+    USERNAME(&'a str),
+    ID(i32),
+}
+
+pub fn find_user<'a>(conn: &PgConnection, key: UserKey<'a>) -> Result<User> {
+    match key {
+        UserKey::USERNAME(name) => users::table
+            .filter(users::username.eq(name))
+            .select((users::id, users::username))
+            .first::<User>(conn)
+            .map_err(AppError::from),
+
+        UserKey::ID(id) => users::table
+            .find(id)
+            .select((users::id, users::username))
+            .first::<User>(conn)
+            .map_err(Into::into),
+    }
 }
