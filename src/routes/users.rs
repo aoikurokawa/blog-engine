a0c6ccc65c04092c38ca04b17fde13a326fe4648
Aoi <ht.so.ik0102@gmail.com>
2021-12-13 08:04:45 +0700

    create_user

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,7 +1,7 @@
 use crate::errors::AppError;
 use crate::routes::convert;
 use crate::{models, Pool};
-use actix_web::{web, HttpResponse};
+use actix_web::{web, HttpResponse, Responder};
 use futures::future::Future;
 // use futures_util::future::future::FutureExt;
 use serde::{Deserialize, Serialize};
@@ -11,26 +11,28 @@ struct UserInput {
     username: String,
 }
 
-async fn create_user(
-    item: web::Json<UserInput>,
-    pool: web::Data<Pool>,
-) -> impl Future<Item = HttpResponse, Error = AppError> {
-    web::block(move || {
-        let conn = &pool.get().unwrap();
-        let username = item.into_inner().username;
-        models::create_user(conn, username.as_str())
-    })
-    .await.and_then(convert)
-}
-
-// fn create_user(
+// async fn create_user(
 //     item: web::Json<UserInput>,
 //     pool: web::Data<Pool>,
-// ) -> impl Future<Output = HttpResponse> {
+// ) -> impl Future<Item = HttpResponse, Error = AppError> {
 //     web::block(move || {
 //         let conn = &pool.get().unwrap();
 //         let username = item.into_inner().username;
 //         models::create_user(conn, username.as_str())
 //     })
-//     .poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+//     .then(|res| {
+//         res.map(|d| HttpResponse::Ok().json(d)).map_err(Into::into)
+//     })
 // }
+
+fn create_user(item: web::Json<UserInput>, pool: web::Data<Pool>) -> impl Responder {
+    web::block(move || {
+        let conn = &pool.get().unwrap();
+        let username = item.into_inner().username;
+        models::create_user(conn, username.as_str())
+    })
+    .then(convert)
+    // .poll(self: Pin<&mut Self>, cx: &mut Context<'_>)
+}
+
+
