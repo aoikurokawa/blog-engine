2c6a57fab2bf32b35648bf72c6fcd8ef26cd355f
Aoi <ht.so.ik0102@gmail.com>
2021-12-16 21:32:31 +0700

    struct app

--- hurl/src/app.rs
+++ hurl/src/app.rs
@@ -3,3 +3,30 @@ use std::convert::TryFrom;
 use structopt::StructOpt;
 
 use crate::errors::{Error, HurlResult};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "hurl")]
+pub struct App {
+    #[structopt(short, long)]
+    pub quiet: bool,
+
+    #[structopt(short, long, parse(from_occurences))]
+    pub verbose: u8,
+
+    #[structopt(short, long)]
+    pub form: bool,
+
+    #[structopt(short, long)]
+    pub auth: Option<String>,
+
+    #[structopt(short, long)]
+    pub secure: bool,
+
+    #[structopt(subcommand)]
+    pub cmd: Option<Method>,
+
+    pub url: Option<String>,
+
+    #[structopt(parse(try_from_str = parse_param))]
+    pub parameters: Vec<Parameter>,
+}
