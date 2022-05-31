0b6387cf4fd1335720f782c7d55499d51922612b
Aoi <ht.so.ik0102@gmail.com>
2022-05-31 08:18:00 +0700

    Change to graphql

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,14 @@
 extern crate juniper;
 extern crate env_logger;
 
-// mod schema;
+mod schema;
 // mod graphql_schema;
 // mod type_defs;
 // mod graphql;
 
-use std::{io, env};
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
+use std::{env, io};
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
@@ -19,15 +19,12 @@ async fn main() -> io::Result<()> {
     env_logger::init();
 
     println!("Starting http server: 127.0.0.1:8080");
-    HttpServer::new(|| {
-        App::new().route("/", web::get().to(index))
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/", web::get().to(index)))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
 
 async fn index() -> impl Responder {
     HttpResponse::Ok().body("Hello World")
 }
-
--- src/schema.rs
+++ src/schema.rs
@@ -1,10 +1,10 @@
-#[macro_use]
-extern crate juniper;
+// #[macro_use]
+// extern crate juniper;
 
-use juniper::{RootNode, http::graphql};
+use juniper::RootNode;
 
-#[derive(GraphQLObject)]
-#[graphql(description="Todo struct")]
+// #[derive(GraphQLObject)]
+// #[graphql(description="Todo struct")]
 struct Todo {
     id: i32,
     title: String,
@@ -12,8 +12,7 @@ struct Todo {
     completed: bool,
 }
 
-
-#[juniper::object(description = "A todo")]
+#[juniper::graphql_object(description = "A todo")]
 impl Todo {
     pub fn id(&self) -> i32 {
         self.id
@@ -34,7 +33,8 @@ impl Todo {
 
 pub struct QueryRoot;
 
-#[juniper::object]
+// dummy data
+#[juniper::graphql_object]
 impl QueryRoot {
     fn todos() -> Vec<Todo> {
         vec![
@@ -42,17 +42,46 @@ impl QueryRoot {
                 id: 1,
                 title: "Watching Backetball".to_string(),
                 description: "Watching the NBA finals".to_string(),
-               completed: false, 
+                completed: false,
             },
             Todo {
                 id: 2,
                 title: "Watching football".to_string(),
                 description: "Watching the NFL finals".to_string(),
-               completed: false, 
-            }
+                completed: false,
+            },
         ]
     }
 }
 
+pub struct MutationRoot;
 
+#[derive(juniper::GraphQLInputObject)]
+pub struct NewTodo {
+    pub title: String,
+    pub description: String,
+    pub completed: bool,
+}
 
+#[juniper::graphql_object]
+impl MutationRoot {
+    fn create_todo(new_todo: NewTodo) -> Todo {
+        Todo {
+            id: 1,
+            title: new_todo.title,
+            description: new_todo.description,
+            completed: new_todo.completed,
+        }
+    }
+}
+
+pub struct SubscriptionRoot;
+
+#[juniper::graphql_object]
+impl SubscriptionRoot {}
+
+pub type Schema = RootNode<'static, QueryRoot, MutationRoot, SubscriptionRoot>;
+
+pub fn create_schema() -> Schema {
+    return Schema::new(QueryRoot, MutationRoot, SubscriptionRoot);
+}
