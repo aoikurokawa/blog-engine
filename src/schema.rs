8b45c8205777164e077cd38fe1a572d06d8ce923
Aoi <ht.so.ik0102@gmail.com>
2022-06-05 18:51:54 +0700

    Update schema and script

--- src/schema.rs
+++ src/schema.rs
@@ -1,7 +1,24 @@
 table! {
+    posts (id) {
+        id -> Int4,
+        user_id -> Int4,
+        title -> Varchar,
+        body -> Text,
+        published -> Bool,
+    }
+}
+
+table! {
     users (id) {
         id -> Int4,
         username -> Varchar,
         email -> Varchar,
     }
 }
+
+joinable!(posts -> users (user_id));
+
+allow_tables_to_appear_in_same_query!(
+    posts,
+    users,
+);
