ec9a1faa79a06542e57bc8f9b8d33952fb146821
Aoi <ht.so.ik0102@gmail.com>
2022-06-05 21:22:57 +0700

    Add comments table

--- src/schema.rs
+++ src/schema.rs
@@ -1,4 +1,13 @@
 table! {
+    comments (id) {
+        id -> Int4,
+        user_id -> Int4,
+        post_id -> Int4,
+        body -> Text,
+    }
+}
+
+table! {
     posts (id) {
         id -> Int4,
         user_id -> Int4,
@@ -16,9 +25,12 @@ table! {
     }
 }
 
+joinable!(comments -> posts (post_id));
+joinable!(comments -> users (user_id));
 joinable!(posts -> users (user_id));
 
 allow_tables_to_appear_in_same_query!(
+    comments,
     posts,
     users,
 );
