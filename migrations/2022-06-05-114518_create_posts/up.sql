5ac4fd3926096e75065c9252e6ee03d215010c5d
Aoi <ht.so.ik0102@gmail.com>
2022-07-04 14:17:39 +0700

    Create new posts table

--- migrations/2022-06-05-114518_create_posts/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-CREATE TABLE posts (
-    id SERIAL PRIMARY KEY NOT NULL,
-    category_id INTEGER NOT NULL REFERENCES categories (id),
-    title VARCHAR NOT NULL,
-    body TEXT NOT NULL,
-    published BOOLEAN NOT NULL DEFAULT FALSE
-);
--- src/schema.rs
+++ src/schema.rs
@@ -6,39 +6,18 @@ table! {
 }
 
 table! {
-    comments (id) {
-        id -> Int4,
-        user_id -> Int4,
-        post_id -> Int4,
-        body -> Text,
-    }
-}
-
-table! {
     posts (id) {
         id -> Int4,
-        user_id -> Int4,
+        category_id -> Int4,
         title -> Varchar,
         body -> Text,
         published -> Bool,
     }
 }
 
-table! {
-    users (id) {
-        id -> Int4,
-        username -> Varchar,
-        email -> Varchar,
-    }
-}
-
-joinable!(comments -> posts (post_id));
-joinable!(comments -> users (user_id));
-joinable!(posts -> users (user_id));
+joinable!(posts -> categories (category_id));
 
 allow_tables_to_appear_in_same_query!(
     categories,
-    comments,
     posts,
-    users,
 );
