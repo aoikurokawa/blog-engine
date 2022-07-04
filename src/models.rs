6db17561cefe198d6d9264b3a3c115b7b7f21d56
Aoi <ht.so.ik0102@gmail.com>
2022-07-04 19:50:57 +0700

    Remove unused file and comment

--- src/models.rs
+++ src/models.rs
@@ -1,17 +1,6 @@
 use crate::schema::posts;
 use crate::schema::categories;
-use diesel::prelude::*;
-use diesel::result::Error;
 use serde_derive::{Deserialize, Serialize};
-
-
-// pub fn find_user(conn: &PgConnection, id: i32) -> Result<User, Error> {
-//     users::table
-//         .find(id)
-//         .select((users::username, users::email))
-//         .first::<User>(conn)
-//         .map_err(Into::into)
-// }
 
 #[derive(Queryable, Associations, Serialize, Debug, Insertable)]
 #[belongs_to(Category)]
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,7 +1,3 @@
-pub mod comments;
 pub mod posts;
-pub mod users;
 
-pub use comments::*;
 pub use posts::*;
-pub use users::*;
