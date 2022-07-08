fdc61c84dc08d727d57efea3310e1f55343d5c02
Aoi <ht.so.ik0102@gmail.com>
2022-07-08 08:15:55 +0700

    Update migration

--- migrations/2022-07-04-065620_create_posts/up.sql
+++
@@ -1,8 +0,0 @@
--- Your SQL goes here
-create table posts (
-    id serial primary key not null,
-    category_id integer not null references categories (id),
-    title varchar not null,
-    body text not null,
-    published boolean not null default false
-);
