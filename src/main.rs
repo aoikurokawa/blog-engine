0d0a71133ae7bce0e1349e8616875b937b13f995
Aoi <ht.so.ik0102@gmail.com>
2022-07-09 16:56:56 +0700

    Update

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 extern crate diesel;
 extern crate serde;
 
+pub mod auth;
 pub mod db;
 pub mod errors;
 pub mod models;
@@ -10,9 +11,30 @@ pub mod schema;
 
 use actix_cors::Cors;
 use actix_web::web::Data;
-use actix_web::{App, HttpServer};
+use actix_web::{dev::ServiceRequest, App, Error, HttpServer};
+use actix_web_httpauth::extractors::bearer::{BearerAuth, Config};
+use actix_web_httpauth::extractors::AuthenticationError;
+use actix_web_httpauth::middleware::HttpAuthentication;
 use dotenv::dotenv;
 use std::io;
+
+async fn validator(req: ServiceRequest, credentials: BearerAuth) -> Result<ServiceRequest, Error> {
+    let config = req
+        .app_data::<Config>()
+        .map(|data| data.as_ref().clone())
+        .unwrap_or_else(Default::default());
+
+    match auth::validate_token(credentials.token()) {
+        Ok(res) => {
+            if res == true {
+                Ok(req)
+            } else {
+                Err(AuthenticationError::from(config).into())
+            }
+        }
+        Err(_) => Err(AuthenticationError::from(config).into()),
+    }
+}
 
 #[actix_web::main]
 async fn main() -> io::Result<()> {
@@ -26,7 +48,9 @@ async fn main() -> io::Result<()> {
     let port = std::env::var("PORT").expect("Port not set");
 
     HttpServer::new(move || {
+        let auth = HttpAuthentication::bearer(validator);
         App::new()
+            .wrap(auth)
             .app_data(Data::new(pool.clone()))
             .configure(routes::posts::configure)
             .configure(routes::categoris::configure)
