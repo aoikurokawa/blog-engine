96e3079044fee33a04e02ba951d5a67fc4ba4bbd
Aoi <ht.so.ik0102@gmail.com>
2022-07-09 11:01:13 +0700

    Add each function

--- src/routes/users.rs
+++ src/routes/users.rs
@@ -1,8 +1,11 @@
-use crate::models::{NewUser, User};
 use crate::schema::users::dsl::*;
-use serde::{Deserialize, Serialize};
-
+use crate::{
+    db,
+    models::{NewUser, User},
+};
 use actix_web::{delete, get, post, put, web, Error, HttpResponse, Responder, Result};
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
     cfg.service(get_users)
@@ -19,13 +22,24 @@ pub struct InputUser {
 }
 
 #[get("/users")]
-async fn get_users() -> impl Responder {
-    format!("hello from get users")
+async fn get_users(db: web::Data<db::Pool>) -> Result<HttpResponse, Error> {
+    Ok(web::block(move || get_all_users(db))
+        .await
+        .map(|user| HttpResponse::Ok().json(user))
+        .map_err(|_| HttpResponse::InternalServerError())?)
 }
 
 #[get("/users/{id}")]
-pub async fn get_user_by_id() -> impl Responder {
-    format!("hello from get users by id")
+pub async fn get_user_by_id(
+    db: web::Data<db::Pool>,
+    user_id: web::Path<i32>,
+) -> Result<HttpResponse, Error> {
+    Ok(
+        web::block(move || db_get_user_by_id(db, user_id.into_inner()))
+            .await
+            .map(|user| HttpResponse::Ok().json(user))
+            .map_err(|_| HttpResponse::InternalServerError())?,
+    )
 }
 
 #[post("/users")]
@@ -36,4 +50,44 @@ pub async fn add_user() -> impl Responder {
 #[delete("/users/{id}")]
 pub async fn delete_user() -> impl Responder {
     format!("hello from delete user")
+}
+
+fn get_all_users(db: web::Data<db::Pool>) -> Result<Vec<User>, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let items = users.load::<User>(&conn)?;
+    Ok(items)
+}
+
+fn db_get_user_by_id(
+    pool: web::Data<db::Pool>,
+    user_id: i32,
+) -> Result<User, diesel::result::Error> {
+    let conn = pool.get().unwrap();
+    users.find(user_id).get_result::<User>(&conn)
+}
+
+fn add_single_user(
+    db: web::Data<db::Pool>,
+    item: web::Json<InputUser>,
+) -> Result<User, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let new_user = NewUser {
+        first_name: &item.first_name,
+        last_name: &item.last_name,
+        email: &item.email,
+        create_at: chrono::Local::now().naive_local(),
+    };
+    let res = diesel::insert_into(users)
+        .values(&new_user)
+        .get_result(&conn)?;
+    Ok(res)
+}
+
+fn delete_single_user(
+    db: web::Data<db::Pool>,
+    user_id: i32,
+) -> Result<usize, diesel::result::Error> {
+    let conn = db.get().unwrap();
+    let count = diesel::delete(users.find(user_id)).execute(&conn)?;
+    Ok(count)
 }
