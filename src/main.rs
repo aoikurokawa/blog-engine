6b08d97ee1c581ddd72b709b5e939179dcbe8247
Aoi <ht.so.ik0102@gmail.com>
2022-08-05 16:22:49 +0700

    Fix the problem

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,6 @@ async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect_lazy(&configuration.database.connection_string())
-        .await
         .expect("Failed to connect Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
