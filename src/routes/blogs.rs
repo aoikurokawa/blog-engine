f758e813d6539e8f9f7ac500cbf080aa35248df8
Aoi <ht.so.ik0102@gmail.com>
2022-08-05 14:45:19 +0700

    Update post and get health_check

--- src/routes/blogs.rs
+++ src/routes/blogs.rs
@@ -8,26 +8,25 @@ pub struct FormData {
 }
 
 pub async fn post_blog(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    // match sqlx::query!(
-    //     r#"
-    //         INSERT INTO blogs (id, title, content, created_at)
-    //         VALUES ($1, $2, $3, $4)
-    //     "#,
-    //     uuid::Uuid::new_v4(),
-    //     form.title,
-    //     form.content,
-    //     chrono::Utc::now()
-    // )
-    // .execute(pool.as_ref())
-    // .await
-    // {
-    //     Ok(_) => HttpResponse::Ok().finish(),
-    //     Err(e) => {
-    //         println!("Failed to execute query: {}", e);
-    //         HttpResponse::InternalServerError().finish()
-    //     }
-    // }
-    HttpResponse::Ok().finish()
+    match sqlx::query!(
+        r#"
+            INSERT INTO blogs (id, title, content, created_at)
+            VALUES ($1, $2, $3, $4)
+        "#,
+        uuid::Uuid::new_v4(),
+        form.title,
+        form.content,
+        chrono::Utc::now()
+    )
+    .execute(pool.as_ref())
+    .await
+    {
+        Ok(_) => HttpResponse::Ok().finish(),
+        Err(e) => {
+            println!("Failed to execute query: {}", e);
+            HttpResponse::InternalServerError().finish()
+        }
+    }
 }
 
 pub async fn fetch_all_blogs() -> HttpResponse {
