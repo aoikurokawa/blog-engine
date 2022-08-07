9decf026a68dd2b2c16ec3f2913fba157068412e
Aoi <ht.so.ik0102@gmail.com>
2022-08-07 10:32:44 +0700

    Modify host and port

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use defistory::configuration::get_configuration;
+use defistory::configuration::{get_configuration, Environment};
 use defistory::startup::run;
 use env_logger::Env;
 use secrecy::ExposeSecret;
@@ -8,10 +8,23 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let configuration = get_configuration().expect("Failed to read configuration.");
+    let mut configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool =
         PgPool::connect_lazy(&configuration.database.connection_string().expose_secret())
             .expect("Failed to connect Postgres.");
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    match environment {
+        Environment::Production => {
+            configuration.application.port = std::env::var("PORT")
+                .expect("Failed to fined PORT.")
+                .parse::<u16>()
+                .unwrap()
+        }
+        Environment::Local => {}
+    }
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
