2e36336366491d4215f216644a9048e0d987c7fe
Aoi <ht.so.ik0102@gmail.com>
2022-08-10 15:45:01 +0700

    Implement get_by_id

--- src/routes/blogs/get.rs
+++ src/routes/blogs/get.rs
@@ -24,3 +24,49 @@ async fn select_all_blogs(pool: &PgPool) -> Result<Vec<Blog>, sqlx::Error> {
         .await?;
     Ok(rows)
 }
+
+// #[derive(Deserialize)]
+// pub struct BlogId {
+//     blog_id: uuid::Uuid,
+// }
+
+pub async fn get_blog_by_id(
+    pool: web::Data<PgPool>,
+    path: web::Path<(uuid::Uuid,)>,
+) -> HttpResponse {
+    let blog_id = path.into_inner();
+    match select_blog_by_id(&pool, blog_id.0).await {
+        Ok(blog) => HttpResponse::Ok().json(blog),
+        Err(_) => HttpResponse::InternalServerError().finish(),
+    }
+}
+
+async fn select_blog_by_id(pool: &PgPool, blog_id: uuid::Uuid) -> Result<Blog, sqlx::Error> {
+    let row = sqlx::query!(
+        r#"SELECT id, title, content, created_at FROM blogs WHERE id = $1"#,
+        blog_id
+    )
+    .fetch_one(pool)
+    .await?;
+
+    let selected_blog = Blog {
+        id: row.id,
+        title: row.title,
+        content: row.content,
+        created_at: row.created_at,
+    };
+
+    Ok(selected_blog)
+
+    // if result.is_some() {
+    //     let selected_blog: Blog = result.unwrap();
+    //     Ok(Some(Blog {
+    //         id: selected_blog.id,
+    //         title: selected_blog.title,
+    //         content: selected_blog.content,
+    //         created_at: selected_blog.created_at,
+    //     }))
+    // } else {
+    //     Ok(None)
+    // }
+}
