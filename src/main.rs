5f84191cf1975f7ff269f45affdd3936240c4cf2
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-21 22:01:21 +0700

    Return simple hello

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-
-
-use std::net::TcpListener;
-
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream}, fs,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -9,6 +9,23 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("./posts/hello.html").unwrap();
+    let length = contents.len();
+
+    let res = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(res.as_bytes()).unwrap();
 }
