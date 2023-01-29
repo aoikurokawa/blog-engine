690b10dabdea695e001301ca40bdc75de6d8ec61
Aoi1011 <ht.so.ik0102@gmail.com>
2023-01-29 22:45:20 +0700

    Use chi

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
 )
 
 type Router struct{}
@@ -18,9 +20,14 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprintf(w, "<h1>FAQ Page</h1><p></p>")
+}
+
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"mailto:jon@calhoun.io\">jon@calhoun.io</a>.</p>")
+	fmt.Fprintf(w, "<h1>Contact Page</h1><p>To get in touch, email me at <a href=\"ghghdgd@gmail.com\">ghghdgd@gmail.com</a>.</p>")
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
@@ -29,8 +36,13 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", homeHandler)
-	http.HandleFunc("/contact", contactHandler)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Page not found", http.StatusNotFound)
+	})
 	fmt.Println("Starting the server on :3000...")
 	http.ListenAndServe(":3000", nil)
 }
