40a73721c7725ddfb7012e7c18de2daea7c56031
aoikurokawa <ht.so.ik0102@gmail.com>
2023-08-27 17:11:59 +0900

    feat: init new blog in Rust

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use std::net::TcpListener;
+
+use actix_web::{dev::Server, middleware, web, App, HttpResponse, HttpServer};
+use lazy_static::lazy_static;
+use tera::Tera;
+
+lazy_static! {
+    pub static ref TEMPLATES: Tera = {
+        let mut tera = match Tera::new("templates/**/*.html") {
+            Ok(t) => t,
+            Err(e) => {
+                println!("Parsing error(s): {}", e);
+                ::std::process::exit(1);
+            }
+        };
+        tera.autoescape_on(vec![".html", ".sql"]);
+        tera
+    };
+}
+
+pub fn start_blog(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let srv = HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(TEMPLATES.clone()))
+            .wrap(middleware::Logger::default())
+            .route("/health", web::get().to(HttpResponse::Ok))
+    })
+    .listen(listener)?
+    .run();
+
+    Ok(srv)
+}
