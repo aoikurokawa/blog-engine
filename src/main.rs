d2d6d378acb9e56ef68ebf3e34b2d5b25e43e7ce
aoikurokawa <ht.so.ik0102@gmail.com>
2023-10-02 07:48:49 +0900

    feat: fix bug opening post detail

--- src/main.rs
+++ src/main.rs
@@ -2,28 +2,39 @@ use std::net::SocketAddr;
 
 use blog::handlers;
 use tokio;
+use tracing_subscriber::fmt::format::FmtSpan;
 use warp::Filter;
 
 #[tokio::main]
 async fn main() {
-    std::env::set_var("RUST_LOG", "actix_web=info");
-    env_logger::init();
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+    // env_logger::init();
+
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
 
     let static_files = warp::path("static").and(warp::fs::dir("static/"));
 
-    let health_check = warp::path!("health_check").map(|| warp::reply::html("OK"));
+    let health_check = warp::path!("health_check")
+        .map(|| warp::reply::html("OK"))
+        .with(warp::trace::named("health_check"));
 
-    let index = warp::path::end().and(warp::get()).and_then(handlers::index);
+    let index = warp::path::end()
+        .and(warp::get())
+        .and_then(handlers::index)
+        .with(warp::trace::named("index"));
 
-    let post = warp::path!("post")
+    let post = warp::path("posts")
         .and(warp::path::param::<String>())
         .and(warp::get())
-        .and_then(handlers::post);
+        .and_then(handlers::post)
+        .with(warp::trace(
+            |info| tracing::info_span!("post", req.path = ?info.path()),
+        ));
 
-    let routes = static_files
-        .or(health_check)
-        .or(index)
-        .or(post);
+    let routes = static_files.or(health_check).or(index).or(post);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     warp::serve(routes).run(addr).await;
