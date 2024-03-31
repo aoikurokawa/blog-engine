8f19e29ad46bc0979a155d6e60d6f595c4eff953
aoikurokawa <ht.so.ik0102@gmail.com>
2024-04-01 08:28:59 +0900

    feat: revert

--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,39 @@
-use std::{io, net::{SocketAddr, TcpListener}};
+use std::net::SocketAddr;
 
-use blog::{handlers, runtime, Async};
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    net::TcpListener,
-};
+use blog::handlers;
 use tracing_subscriber::fmt::format::FmtSpan;
 use warp::Filter;
 
-fn main() -> io::Result<()> {
-    let runtime = tokio::runtime::Runtime::new()?;
-    let _guard = runtime.enter();
+#[tokio::main]
+async fn main() {
+    let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
 
-    let mut executor = runtime::init()?;
-    executor.block_on(start_heart_beat());
+    tracing_subscriber::fmt()
+        .with_env_filter(filter)
+        .with_span_events(FmtSpan::CLOSE)
+        .init();
 
-    Ok(())
-    // let filter = std::env::var("RUST_LOG").unwrap_or_else(|_| "tracing=info,warp=debug".to_owned());
+    let static_files = warp::path("static").and(warp::fs::dir("static/"));
 
-    // tracing_subscriber::fmt()
-    //     .with_env_filter(filter)
-    //     .with_span_events(FmtSpan::CLOSE)
-    //     .init();
+    let health_check = warp::path!("health_check")
+        .map(|| warp::reply::html("OK"))
+        .with(warp::trace::named("health_check"));
 
-    // let static_files = warp::path("static").and(warp::fs::dir("static/"));
+    let index = warp::path::end()
+        .and(warp::get())
+        .and_then(handlers::index)
+        .with(warp::trace::named("index"));
 
-    // let health_check = warp::path!("health_check")
-    //     .map(|| warp::reply::html("OK"))
-    //     .with(warp::trace::named("health_check"));
+    let post = warp::path("posts")
+        .and(warp::path::param::<String>())
+        .and(warp::get())
+        .and_then(handlers::post)
+        .with(warp::trace(
+            |info| tracing::info_span!("post", req.path = ?info.path()),
+        ));
 
-    // let index = warp::path::end()
-    //     .and(warp::get())
-    //     .and_then(handlers::index)
-    //     .with(warp::trace::named("index"));
+    let routes = static_files.or(health_check).or(index).or(post);
 
-    // let post = warp::path("posts")
-    //     .and(warp::path::param::<String>())
-    //     .and(warp::get())
-    //     .and_then(handlers::post)
-    //     .with(warp::trace(
-    //         |info| tracing::info_span!("post", req.path = ?info.path()),
-    //     ));
-
-    // let routes = static_files.or(health_check).or(index).or(post);
-
-    // let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    // warp::serve(routes).run(addr).await;
-}
-
-async fn start_heart_beat() -> io::Result<()> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
-    let listener = Async::bind(addr)?;
-
-    loop {
-        let (mut stream, _) = listener.accept().await?;
-
-        let mut buf = [0; 1024];
-        loop {
-            let n = match stream.read(&mut buf).await {
-                Ok(n) if n == 0 => return Err(io::Error::new(io::ErrorKind::NotFound, "")),
-                Ok(n) => n,
-                Err(e) => {
-                    eprintln!("failed to read from stream; err = {:?}", e);
-                    return Err(io::Error::new(io::ErrorKind::NotFound, ""));
-                }
-            };
-
-            println!("{n}");
-            if let Err(e) = stream.write_all(&buf[0..n]).await {
-                eprintln!("failed to write to stream; err = {:?}", e);
-                return Err(io::Error::new(io::ErrorKind::NotFound, ""));
-            }
-        }
-    }
+    warp::serve(routes).run(addr).await;
 }
