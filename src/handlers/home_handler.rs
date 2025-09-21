f74c31d1c2639ec2e4862ae1a8ed423c3d0a456d
aoikurokawa <ht.so.ik0102@gmail.com>
2025-09-22 07:04:57 +0900

    fix: sort

--- src/handlers/home_handler.rs
+++ src/handlers/home_handler.rs
@@ -84,7 +84,7 @@ fn find_all_frontmatters() -> Result<Vec<Frontmatter>, Error> {
             }
             Err(e) => {
                 eprintln!("{:}", e);
-                  return Err(Error::new(
+                return Err(Error::new(
                     std::io::ErrorKind::NotFound,
                     "could not locate frontmatter",
                 ));
